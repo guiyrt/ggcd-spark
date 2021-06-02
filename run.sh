5ef22e40afb09eceb755386fdd91f6e373bd6e4c
daniel <danielregado@gmail.com>
2021-06-02 02:48:39 +0100

    Created run.sh to execute all jobs from scratch

---
+++ run.sh
@@ -0,0 +1,47 @@
+# Any command failure will stop execution
+set -e
+
+# Check if got correct number of inputs
+if [ "$#" -ne 4 ]; then
+  printf "Wrong number of arguments!\nUsage: ./run.sh <title.basics> <title.ratings> <title.principals> <name.basics> \n"
+  exit 128 # 128 is invalid arguments error
+fi
+
+# Check if input parameters are valid
+if ! [ -f "$1" ] && ! [ -f "$2" ] && ! [ -f "$3" ] && ! [ -f "$4" ]; then
+  printf "Invalid files!\nUsage: ./run.sh <title.basics> <title.ratings> <title.principals> <name.basics> \n"
+  exit 128 # 128 is invalid arguments error
+fi
+
+# Define relevant files and paths
+CLUSTER="src/gcloud/cluster.sh"
+JAR="target/ggcd-spark-1.0.jar"
+
+# Compile and generate JAR
+mvn package
+
+# Create cluster
+./"$CLUSTER" create
+
+# Create folders in HDFS
+./"$CLUSTER" hdfs_mkdir /titleBasics
+./"$CLUSTER" hdfs_mkdir /titleRatings
+./"$CLUSTER" hdfs_mkdir /titlaPrincipals
+./"$CLUSTER" hdfs_mkdir /nameBasics
+
+# Upload files to HDFS
+./"$CLUSTER" hdfs_upload "$1" /titleBasics
+./"$CLUSTER" hdfs_upload "$2" /titleRatings
+./"$CLUSTER" hdfs_upload "$3" /titlePrincipals
+./"$CLUSTER" hdfs_upload "$4" /nameBasics
+
+# Run hive jobs
+./"$CLUSTER" hive src/hive/titleBasics.hql
+./"$CLUSTER" hive src/hive/titleRatings.hql
+./"$CLUSTER" hive src/hive/titlePrincipals.hql
+./"$CLUSTER" hive src/hive/nameBasics.hql
+
+# Retrieve output
+./"$CLUSTER" spark "$JAR" Jobs.ShowRddOperations
+./"$CLUSTER" spark "$JAR" Jobs.CreateActorPage
+./"$CLUSTER" spark "$JAR" ActorPage.Actor
--- src/gcloud/cluster.sh
+++ src/gcloud/cluster.sh
@@ -65,26 +65,26 @@ case $1 in
 
   # Submit Spark job
   "spark")
-    argcGe "$#" 3 "Invalid input parameters, spark requires at least 2 parameters.\\nUsage: ./dataproc.sh spark <JARS> <MAIN_CLASS> [JOB ARGUMENTS]"
+    argcGe "$#" 3 "Invalid input parameters, spark requires at least 2 parameters.\\nUsage: ./cluster.sh spark <JARS> <MAIN_CLASS> [JOB ARGUMENTS]"
     gcloud dataproc jobs submit spark --region=$REGION --cluster=$CLUSTER_NAME  --jars="$2" --class="$3" -- "${@:4}"
     ;;
 
   # Submit Hive job
   "hive")
-    argcEq "$#" 2 "Invalid input parameters, hive requires 1 parameter.\\nUsage: ./dataproc.sh hive <ACTION_FILE>"
+    argcEq "$#" 2 "Invalid input parameters, hive requires 1 parameter.\\nUsage: ./cluster.sh hive <ACTION_FILE>"
     ACTION=$(cat "$2")
     gcloud dataproc jobs submit hive --region=$REGION --cluster=$CLUSTER_NAME --execute="\"$ACTION\""
     ;;
 
  # Add file to HDFS
   "hdfs_mkdir")
-    argcEq "$#" 2 "Invalid input parameters, hdfs_mkdir requires 1 parameter.\nUsage: ./dataproc.sh hdfs_mkdir <PATH>"
+    argcEq "$#" 2 "Invalid input parameters, hdfs_mkdir requires 1 parameter.\nUsage: ./cluster.sh hdfs_mkdir <PATH>"
     gcloud compute ssh --zone="$ZONE" "$CLUSTER_NAME"-m --command="hdfs dfs -mkdir $2"
     ;;
 
   # Add file to HDFS
   "hdfs_upload")
-    argcEq "$#" 3 "Invalid input parameters, hdfs_upload requires 2 parameters.\nUsage: ./dataproc.sh hdfs_upload <SRC> <DEST>"
+    argcEq "$#" 3 "Invalid input parameters, hdfs_upload requires 2 parameters.\nUsage: ./cluster.sh hdfs_upload <SRC> <DEST>"
     FILE="$(basename "$2")"
     gcloud compute scp --recurse --zone="$ZONE" "$2" "$CLUSTER_NAME"-m:"$FILE"
     gcloud compute ssh --zone="$ZONE" "$CLUSTER_NAME"-m --command="hdfs dfs -put $FILE $3; rm -r $FILE"
@@ -92,7 +92,7 @@ case $1 in
 
   # Download files from HDFS
   "hdfs_download")
-    argcEq "$#" 3 "Invalid input parameters, hdfs_download requires 2 parameters.\nUsage: ./dataproc.sh hdfs_download <SRC> <DEST>"
+    argcEq "$#" 3 "Invalid input parameters, hdfs_download requires 2 parameters.\nUsage: ./cluster.sh hdfs_download <SRC> <DEST>"
     FILE="$(basename "$2")"
     TIMESTAMP="$(date +%s%N)"
     # shellcheck disable=SC2001
@@ -104,19 +104,19 @@ case $1 in
 
   # Delete file from HDFS
   "hdfs_delete")
-    argcEq "$#" 2 "Invalid input parameters, hdfs_delete requires 1 parameters.\nUsage: ./dataproc.sh hdfs_delete <SRC>"
+    argcEq "$#" 2 "Invalid input parameters, hdfs_delete requires 1 parameters.\nUsage: ./cluster.sh hdfs_delete <SRC>"
     gcloud compute ssh --zone="$ZONE" "$CLUSTER_NAME"-m --command="hdfs dfs -rm -R $2"
     ;;
 
   # List directory in HDFS
   "hdfs_ls")
-    argcEq "$#" 2 "Invalid input parameters, hdfs_ls requires 1 parameters.\nUsage: ./dataproc.sh hdfs_ls <SRC>"
+    argcEq "$#" 2 "Invalid input parameters, hdfs_ls requires 1 parameters.\nUsage: ./cluster.sh hdfs_ls <SRC>"
     gcloud compute ssh --zone="$ZONE" "$CLUSTER_NAME"-m --command="hdfs dfs -ls $2"
     ;;
 
     *)
       # shellcheck disable=SC2059
-      printf "Invalid option!\nUsage: ./dataproc.sh <create|start|stop|delete|spark|hive|hdfs_mkdir|hdfs_upload|hdfs_download|hdfs_delete|hdfs_ls> [ARGS]\n"
+      printf "Invalid option!\nUsage: ./cluster.sh <create|start|stop|delete|spark|hive|hdfs_mkdir|hdfs_upload|hdfs_download|hdfs_delete|hdfs_ls> [ARGS]\n"
       exit $E_INVALID_ARGUMENTS
       ;;
 esac
